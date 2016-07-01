d830955cc741c53c03419efa0da357e5ecb24853
Jorge Sanmartin <J.Sanmartin@cronostelecom.com>
2016-07-01 19:00:12 +0200

    - Update Realm

--- app/src/main/java/com/grability/rappi/model/dataacess/database/RPDatabase.java
+++ app/src/main/java/com/grability/rappi/model/dataacess/database/RPDatabase.java
@@ -2,8 +2,6 @@ package com.grability.rappi.model.dataacess.database;
 
 import com.cronosgroup.core.view.application.BaseApplication;
 
-import io.realm.Realm;
-
 /**
  * Created by jorgesanmartin on 7/1/16.
  */
@@ -12,7 +10,7 @@ public class RPDatabase {
     public static final String DATABASE_NAME = "RappiDatabase";
 
     public static void initDataBase(BaseApplication aplication) {
-        Realm realm = Realm.getInstance(aplication, DATABASE_NAME);
+//        Realm realm = Realm.getInstance(aplication, DATABASE_NAME);
     }
 
 }
